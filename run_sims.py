378da4d0a73e7b2ef038ef451de129990c42e47f
Robyn Stuart <robyna.s@gmail.com>
2026-02-12 06:38:09 -0800

    try again

--- run_sims.py
+++ run_sims.py
@@ -151,14 +151,14 @@ def make_calib(n_trials=None, n_workers=None):
         own_imm_hr=[0.5, 0.25, 1, 0.05],
         cell_imm_init=dict(par1=[0.5, 0.2, 0.8, 0.05]),
         age_risk=dict(risk=[1, 1, 4, 0.1], age=[30, 30, 45, 1]),
-        m_cross_layer=[0.3, 0.1, 0.7, 0.05],
-        m_partners=dict(
-            c=dict(par1=[0.2, 0.1, 0.6, 0.02])
-        ),
-        f_cross_layer=[0.1, 0.05, 0.5, 0.05],
-        f_partners=dict(
-            c=dict(par1=[0.2, 0.1, 0.6, 0.02])
-        ),
+        # m_cross_layer=[0.3, 0.1, 0.7, 0.05],
+        # m_partners=dict(
+        #     c=dict(par1=[0.2, 0.1, 0.6, 0.02])
+        # ),
+        # f_cross_layer=[0.1, 0.05, 0.5, 0.05],
+        # f_partners=dict(
+        #     c=dict(par1=[0.2, 0.1, 0.6, 0.02])
+        # ),
         sev_dist=dict(par1=[1, 0.5, 1.5, 0.01])
     )
 
@@ -264,7 +264,7 @@ if __name__ == '__main__':
     T = sc.timer()  # Start a timer
 
     if 'run_sim' in to_run:
-        calib_pars = sc.loadobj('results/kenya_pars_v2.obj')  # Load parameters from a previous calibration
+        calib_pars = sc.loadobj('results/kenya_pars.obj')  # Load parameters from a previous calibration
         analyzers = [hpv.age_causal_infection(start_year=2020)]
         sim = run_sim(calib_pars=calib_pars, do_save=False, do_shrink=False, analyzers=analyzers)
         df = pac.get_age_causal_df(sim)
