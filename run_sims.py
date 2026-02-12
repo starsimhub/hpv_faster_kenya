b0805db29083358360bd729a18e1c58f8bb4f239
Robyn Stuart <robyna.s@gmail.com>
2026-02-11 16:38:17 -0800

    3k

--- run_sims.py
+++ run_sims.py
@@ -18,8 +18,8 @@ debug = 0  # Run with smaller population sizes and in serial
 do_shrink = True  # Do not keep people when running sims (saves memory)
 
 # Run settings
-n_trials    = [4000, 2][debug]  # How many trials to run for calibration
-n_workers   = [80, 1][debug]    # How many cores to use
+n_trials    = [3000, 2][debug]  # How many trials to run for calibration
+n_workers   = [75, 1][debug]    # How many cores to use
 storage = None
 
 # Save settings
